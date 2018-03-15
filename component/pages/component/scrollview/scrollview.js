e43993dbf5afcb02642483eadd20de8341adecd5
ruthQi <qi0428ru@163.com>
2018-03-15 10:17:04 +0800

    模拟下拉刷新

--- component/pages/component/scrollview/scrollview.js
+++ component/pages/component/scrollview/scrollview.js
@@ -6,7 +6,15 @@ Page({
    * 页面的初始数据
    */
   data: {
-    toView: 'first'
+    toView: 'first',
+    isHidden: true,
+    viewArr: [{
+      unique:'first',value:'first'
+    }, {
+      unique: 'second', value: 'second'
+      }, {
+        unique: 'third', value: 'third'
+      }]
   },
 
   scrolltoupper: function(e){
@@ -14,6 +22,24 @@ Page({
   },
   scrolltolower: function(e){
     console.log('滚动到底部/右边触发', e);
+    //模拟下拉刷新
+    let data = [{
+      unique: 'forth', value: 'forth'
+    }, {
+        unique: 'fifth', value: 'fifth'
+    }, {
+        unique: 'sixth', value: 'sixth'
+    }];
+    let dataArr = this.data.viewArr.concat(data);
+    setTimeout(()=>{
+      this.setData({
+        viewArr: dataArr,
+        isHidden: true
+      })
+    }, 1000)
+    this.setData({
+      isHidden: false
+    })
   },
   scroll: function(e){
     console.log('滚动：', e);
@@ -26,5 +52,14 @@ Page({
     this.setData({
       toView: arr[index]
     })
+    // for(var i=0;i<arr.length;i++){
+    //   if(this.data.toView === arr[i] && i < 2){
+    //     console.log(i)
+    //     this.setData({
+    //       toView: arr[i+1]
+    //     });
+    //     break;
+    //   }
+    // }
   }
 })
