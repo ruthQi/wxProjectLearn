33c1d6a84b9915df2501b046d3c2b0bc61d6be02
ruthQi <qi0428ru@163.com>
2018-03-16 11:33:28 +0800

    添加picker组件

--- component/pages/component/form/form.js
+++ component/pages/component/form/form.js
@@ -12,7 +12,31 @@ Page({
       { name: 'JPN', value: '日本' },
       { name: 'ENG', value: '英国' },
       { name: 'TUR', value: '法国' }
-    ]
+    ],
+    array: ['美国', '中国', '巴西', '日本'],
+    objectArray: [{
+      id: 0,
+      name: '美国'
+    },
+    {
+      id: 1,
+      name: '中国'
+    },
+    {
+      id: 2,
+      name: '巴西'
+    },
+    {
+      id: 3,
+      name: '日本'
+    }],
+    index:0,
+    multiIndex:[0,0,0],
+    multiArray: [['无脊柱动物', '脊柱动物'], ['扁性动物', '线形动物', '环节动物', '软体动物', '节肢动物'], ['猪肉绦虫', '吸血虫']],
+    time:'12:01',
+    date:'2018-03-16',
+    region:['广东省', '广州市', '海珠区'],
+    customItem:'全部'
   },
 
   changeDisabled: function(){
@@ -66,5 +90,108 @@ Page({
       value: value.replace(/11/g, 2),
       cursor:pos
     }
+  },
+  //================picker
+  bindPickerChange:function(e){
+    console.log(e)
+    this.setData({
+      index: e.detail.value
+    })
+  },
+  bindPickerChangeObject: function(e){
+    console.log(e)
+    this.setData({
+      index: e.detail.value
+    })
+  },
+  bindPickerCancel:function(e){
+    console.log(e)
+  },
+  bindMultiPickerChange:function(e){
+    console.log(e)
+    this.setData({
+      multiIndex:e.detail.value
+    })
+  },
+  //联动
+  bindMultiPickerColumnChange:function(e){
+    console.log('修改的列为', e.detail.column, '，值为', e.detail.value);
+    var data = {
+      multiArray: this.data.multiArray,
+      multiIndex: this.data.multiIndex
+    };
+    data.multiIndex[e.detail.column] = e.detail.value;
+    switch (e.detail.column) {
+      case 0:
+        switch (data.multiIndex[0]) {
+          case 0:
+            data.multiArray[1] = ['扁性动物', '线形动物', '环节动物', '软体动物', '节肢动物'];
+            data.multiArray[2] = ['猪肉绦虫', '吸血虫'];
+            break;
+          case 1:
+            data.multiArray[1] = ['鱼', '两栖动物', '爬行动物'];
+            data.multiArray[2] = ['鲫鱼', '带鱼'];
+            break;
+        }
+        data.multiIndex[1] = 0;
+        data.multiIndex[2] = 0;
+        break;
+      case 1:
+        switch (data.multiIndex[0]) {
+          case 0:
+            switch (data.multiIndex[1]) {
+              case 0:
+                data.multiArray[2] = ['猪肉绦虫', '吸血虫'];
+                break;
+              case 1:
+                data.multiArray[2] = ['蛔虫'];
+                break;
+              case 2:
+                data.multiArray[2] = ['蚂蚁', '蚂蟥'];
+                break;
+              case 3:
+                data.multiArray[2] = ['河蚌', '蜗牛', '蛞蝓'];
+                break;
+              case 4:
+                data.multiArray[2] = ['昆虫', '甲壳动物', '蛛形动物', '多足动物'];
+                break;
+            }
+            break;
+          case 1:
+            switch (data.multiIndex[1]) {
+              case 0:
+                data.multiArray[2] = ['鲫鱼', '带鱼'];
+                break;
+              case 1:
+                data.multiArray[2] = ['青蛙', '娃娃鱼'];
+                break;
+              case 2:
+                data.multiArray[2] = ['蜥蜴', '龟', '壁虎'];
+                break;
+            }
+            break;
+        }
+        data.multiIndex[2] = 0;
+        console.log(data.multiIndex);
+        break;
+    }
+    this.setData(data);
+  },
+  bindTimePickerChange:function(e){
+    console.log(e)
+    this.setData({
+      time:e.detail.value
+    })
+  },
+  bindDatePickerChange: function(e){
+    this.setData({
+      date:e.detail.value
+    })
+  },
+  bindRegionPickerChange: function(e){
+    console.log(e);
+    this.setData({
+      region:e.detail.value
+    })
   }
 })
