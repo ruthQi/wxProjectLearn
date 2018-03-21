a0b8f2c6d9112ae0ccd888b2a6f8916c2338f2c6
qiru <bjqiru@corp.netease.com>
2018-03-21 18:35:48 +0800

    添加canvas

--- component/pages/component/canvas/canvas.js
+++ component/pages/component/canvas/canvas.js
@@ -4,62 +4,64 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+      x: 0,
+      y:0,
+      isHidden:true
   },
+  onReady: function(){
+     var firstCtx = wx.createCanvasContext('firstCanvas');
+     //矩形
+     firstCtx.setStrokeStyle('#ff00ff');
+     firstCtx.setLineWidth(5);
+     firstCtx.rect(0,0,200,200);
+     firstCtx.stroke();
+     //笑脸
+     firstCtx.setStrokeStyle('#FF0000');
+     firstCtx.setLineWidth(2);
+     firstCtx.moveTo(160,100);
+     /*
+      arc(x,y,r,ang1,ang2,boolean):
+      x,y表示圆点坐标；
+      r:圆弧半径
+      ang1,ang2:表示圆弧的起止角度；
+      boolean:指定弧度的方向是逆时针还是顺时针。默认是false，即顺时针。
+     */
+     firstCtx.arc(100, 100, 60, 0, 2 * Math.PI, true);
+     firstCtx.moveTo(140, 100);
+     firstCtx.arc(100, 100, 40, 0, Math.PI, false);
+     firstCtx.moveTo(85, 80);
+     firstCtx.arc(80, 80, 5, 0, 2 * Math.PI, true);
+     firstCtx.moveTo(125, 80);
+     firstCtx.arc(120, 80, 5, 0, 2 * Math.PI, true);
+     firstCtx.stroke();
+     firstCtx.draw();
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    
+     //第二个canvas
+   //   var secondCanvas = wx.createCanvasContext('secondCanvas');
+   //   secondCanvas.arc(5,5,5,0,2*Math.PI);
+   //   secondCanvas.setFillStyle('#aaa');
+   //   secondCanvas.fill();
+   //   secondCanvas.draw();
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
+  start: function(e){
+     console.log(e)
+     this.setData({
+        isHidden: false,
+        x: e.touches[0].x,
+        y: e.touches[0].y
+     })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
+  move: function(e){
+     console.log(e)
+     this.setData({
+        x: e.touches[0].x,
+        y: e.touches[0].y
+     })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
+  end: function(e){
+      this.setData({
+         isHidden:true
+      })
   }
+  
 })
