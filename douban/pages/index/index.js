58909aacbcd888848fc8b9decfe9b9f6ec440499
qiru <bjqiru@corp.netease.com>
2018-03-23 15:58:54 +0800

    豆瓣数据展示

---
+++ douban/pages/index/index.js
@@ -0,0 +1,43 @@
+var Url = 'http://localhost/v2/movie/top250';
+// var start = 0;
+// var count = 20;
+var loadingNum = 0;
+Page({
+   data:{
+      movieList:[],
+      title:'加载中...'
+   },
+   onLoad: function(){
+     
+     this.requestFun(0,20);
+   },
+   loadOther: function(e){
+      console.log(e);
+      loadingNum ++;
+      this.requestFun(loadingNum * 20 ,20);
+   },
+   requestFun: function(start,count){
+      wx.showLoading({
+         title: '加载中...',
+      });
+      wx.request({
+         url: Url,
+         data: {
+            start: start,
+            count: count
+         },
+         header: {
+            'content-type': 'json' // 默认值
+         },
+         success: (res) => {
+            console.log(res)
+            wx.hideLoading();
+            this.setData({
+               title: res.data.title,
+               movieList: this.data.movieList.concat(res.data.subjects)
+            })
+         }
+      })
+   }
+  
+})
