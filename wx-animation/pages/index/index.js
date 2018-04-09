3fcd4c117b9776082bf201660efcdbcbce0e2b82
qiru <bjqiru@corp.netease.com>
2018-04-09 19:23:43 +0800

    MOD

--- wx-animation/pages/index/index.js
+++ wx-animation/pages/index/index.js
@@ -1,12 +1,24 @@
 //index.js
 //获取应用实例
 var Hilo = require('../../utils/hilo.js');
+var loader = null;
 Page({
   
   onLoad:function(){
-     this.initView();
+     this.loadImages();
   },
-  initView: function(){
-     
-  }
+  
+  loadImages: function() {
+     loader = new Hilo.LoadQueue();
+     loader.add([
+        { id: 'bg', src: '/images/pages/bg.png' },
+        { id: 'logo', src: '/images/pages/logo.png' },
+        { id: 'restartBtn', src: '/images/pages/btn-restart.png' }
+     ]).on('complete', () => {
+        this.initStage();
+     }).start();
+  },
+  initStage: function () {
+      console.log('000000000')
+  },
 })
